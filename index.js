10e43133ce48868bab65b5665b0926a96ed37ed2
Dominik <dominiktelka@gmail.com>
2022-08-10 12:49:44 +0200

    deleted unnecessary lines of test code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({
     extended: true,
 }));
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(express.json())
 app.engine('.hbs', hbs.engine({
     extname: '.hbs',
@@ -19,13 +19,6 @@ app.set('view engine', '.hbs');
 
 app.use('/', homeRouter)
 app.use('/client', clientRouter)
-app.get('/test',(req,res)=>{
-    db.update("6a2c644a-32cc-449a-b1b0-29075ece2fc2",{
-        "name":"Tester",
-        "maile":"abc@pl.com",
-    })
-    res.send('ok')
-})
 
 app.listen(3000, 'localhost', ()=>{
     console.log('Listening on http://localhost:3000')
