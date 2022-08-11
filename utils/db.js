8eec366ad7a5c4f2bda01f7513dbf7dd5a8653f1
Dominik <dominiktelka@gmail.com>
2022-08-11 11:47:22 +0200

    changed way of getting client id

--- utils/db.js
+++ utils/db.js
@@ -16,10 +16,12 @@ class Db{
     }
 
     create(obj){
+        const id = uuid()
         this._data.push({
-            id: uuid(),
+            id,
             ...obj,})
         this._save();
+        return id;
     }
 
     getAll(){
