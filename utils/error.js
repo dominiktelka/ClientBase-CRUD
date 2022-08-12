349f3acd7e02481038f89818a97a273a72100679
Dominik <dominiktelka@gmail.com>
2022-08-12 13:18:23 +0200

    created class ValidationError to avoid transmitting our error to client. It can show error on client side but not when we made mistake in our program

--- utils/error.js
+++ utils/error.js
@@ -1,9 +1,12 @@
+class ValidationError extends Error{}
+
 function handleError(err, req,res, next){
+
     res.render('error',{
-        message: err.message,
+        message: err instanceof ValidationError ? err.message : 'Sorry, try again later.'
     });
 }
 
 module.exports = {
-    handleError
+    handleError, ValidationError
 }
